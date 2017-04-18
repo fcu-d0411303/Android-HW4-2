9b2a8350432e4537bbb79e8b510b019720e8539a
fcu-d0411303 <msjeib1076FS>
2017-04-18 21:11:08 +0800

    HW4 2 1

---
+++ app/src/main/java/com/example/user/myapplication2/GreetingActivity.java
@@ -0,0 +1,15 @@
+package com.example.user.myapplication2;
+
+import android.os.Bundle;
+
+/**
+ * Created by user on 2017/4/18.
+ */
+
+public class GreetingActivity extends MainActivity {
+
+    @Override
+        protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
