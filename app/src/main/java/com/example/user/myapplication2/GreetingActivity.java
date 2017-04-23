869bc6a3f758a73413ba3b8f14f34af94eefe4f2
fcu-d0411303 <msjeib1076FS>
2017-04-24 01:37:01 +0900

    hw 4 2 2

--- app/src/main/java/com/example/user/myapplication2/GreetingActivity.java
+++ app/src/main/java/com/example/user/myapplication2/GreetingActivity.java
@@ -1,15 +1,31 @@
 package com.example.user.myapplication2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 /**
  * Created by user on 2017/4/18.
  */
 
-public class GreetingActivity extends MainActivity {
+public class GreetingActivity extends AppCompatActivity {
 
     @Override
         protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView tv = (TextView)findViewById(R.id.say_hello);
+        Intent intent = getIntent();
+        String hello = "Hello ";
+        if(intent != null) {
+            String name = intent.getStringExtra("Name");
+            if(name != null) {
+                hello = hello + name;
+            }
+        }
+        tv.setText(hello);
     }
 }
--- app/src/main/java/com/example/user/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication2/MainActivity.java
@@ -1,57 +1,62 @@
 package com.example.user.myapplication2;
 
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import static com.example.user.myapplication2.R.id.toolbar;
 
+public class MainActivity extends BroadcastReceiver{
+
+    static int id = 70000;
     @Override
-    protected void onReceive(Context context, Intent intent) {
+    public void onReceive(Context context, Intent intent) {
 
-        String msg = intent.getStringExtra("KEY_MSG");
+        String name = intent.getStringExtra("Name");
 
         Intent newintent = new Intent();
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        newintent.setClass(context, GreetingActivity.class);
+        newintent.putExtra("Name", name);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, newintent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        Notification notify = newNotification(context, pendingIntent,
+                "(New) Broadcast is received.", name);
+
+        NotificationManager notificationManager = (NotificationManager)context.
+                getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(id++, notify);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    @SuppressLint("NewApi")
+    private Notification newNotification(
+            Context context, PendingIntent pi,
+            String title, String msg) {
+
+        Notification.Builder builder =
+                new Notification.Builder(context);
+
+        builder.setContentTitle("Hello");
+        builder.setContentText(msg);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setContentIntent(pi);
+        builder.setTicker(msg);
+        builder.setWhen(System.currentTimeMillis());
+        Notification notify = builder.build();
+        return notify;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
